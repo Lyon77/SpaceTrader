b6829a4d340873168a669470937086b506921684
RLBlaze77 <rli342@gatech.edu>
2019-02-15 14:13:38 -0500

    Player added to Game file

--- app/src/main/java/com/example/rjl77/spacetrader/game/Game.java
+++ app/src/main/java/com/example/rjl77/spacetrader/game/Game.java
@@ -1,13 +1,21 @@
 package com.example.rjl77.spacetrader.game;
 
+import com.example.rjl77.spacetrader.entities.Player;
+
 public class Game {
     private String level;
+    private Player p;
 
-    public Game(String level) {
+    public Game(String level, Player p) {
         this.level = level;
+        this.p = p;
     }
 
     public String getLevel() {
         return this.level;
+    }
+
+    public Player getPlayer() {
+        return this.p;
     }
 }
