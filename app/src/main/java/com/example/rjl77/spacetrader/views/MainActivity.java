ff53359105b6243f320614892157cafe62c318ee
RLBlaze77 <rli342@gatech.edu>
2019-02-17 10:26:43 -0500

    Upload MainActivity.java and vcs.xml

---
+++ app/src/main/java/com/example/rjl77/spacetrader/views/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.rjl77.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.rjl77.spacetrader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Gets the button to create the player
+        Button create =  findViewById(R.id.button_create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add the code here to check for player points
+            }
+        });
+
+        //button to exit the game and close the app
+        Button exit =  findViewById(R.id.button_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
