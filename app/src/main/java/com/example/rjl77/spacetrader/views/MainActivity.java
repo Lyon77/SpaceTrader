702e52555ba9c947b9c671d0266f248c54e96ef5
RLBlaze77 <rli342@gatech.edu>
2019-02-17 13:31:44 -0500

    Added Log to MainActivity

--- app/src/main/java/com/example/rjl77/spacetrader/views/MainActivity.java
+++ app/src/main/java/com/example/rjl77/spacetrader/views/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rjl77.spacetrader.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Add the code here to check for player points
+                Log.i("Check", "Create Button Works");
             }
         });
 
