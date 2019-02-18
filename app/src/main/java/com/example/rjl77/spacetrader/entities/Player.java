59419fccf62bfb5f6484e11d411a7f9fb60cf6f6
Jonathan He <jonathan@Jonathans-Mac.local>
2019-02-17 20:41:14 -0500

    added check to confirm attributes sum to 16

--- app/src/main/java/com/example/rjl77/spacetrader/entities/Player.java
+++ app/src/main/java/com/example/rjl77/spacetrader/entities/Player.java
@@ -10,11 +10,15 @@ public class Player {
     private int engineer;
 
     public Player() {
-        this("Ryan");
+        this("Ryan", 4, 4, 4, 4);
     }
 
-    public Player(String name) {
+    public Player(String name, int pil, int fight, int trade, int eng) {
         this.name = name;
+        this.pilot = pil;
+        this.fighter = fight;
+        this.trader = trade;
+        this.engineer = eng;
         this.credits = 1000;
         this.ship = new Ship();
     }
@@ -34,6 +38,8 @@ public class Player {
 //    @androidx.annotation.NonNull
     @Override
     public String toString() {
-        return "Name: " + this.name + " " + "Credits: " + credits + " " + "Ship: " + ship.toString();
+        return "Name: " + this.name + "\n" + "Credits: " + credits + "\n" + "Ship: " + ship.toString()
+                + "\n" + "Pilot Skill: " + pilot + "\n" + "Fighter Skill: " + fighter + "\n" + "Trader Skill: "
+                + trader + "\n" + "Engineer Skill: " + engineer + "\n";
     }
 }
--- app/src/main/java/com/example/rjl77/spacetrader/views/MainActivity.java
+++ app/src/main/java/com/example/rjl77/spacetrader/views/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 import com.example.rjl77.spacetrader.R;
 
@@ -21,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Add the code here to check for player points
-                Log.i("Check", "Create Button Works");
+                try {
+                    EditText pilot_skill = findViewById(R.id.pilot_skill_points);
+                    int pilot = Integer.parseInt(pilot_skill.getText().toString());
+                    EditText fighter_skill = findViewById(R.id.fighter_skill_points);
+                    int fighter = Integer.parseInt(fighter_skill.getText().toString());
+                    EditText trader_skill = findViewById(R.id.trader_skill_points);
+                    int trader = Integer.parseInt(trader_skill.getText().toString());
+                    EditText engineer_skill = findViewById(R.id.engineer_skill_points);
+                    int engineer = Integer.parseInt(engineer_skill.getText().toString());
+
+                    if (pilot + fighter + trader + engineer != 16) {
+                        Log.i("Confirm sum to 16", "Does not sum to 16");
+                    }
+                    Log.i("Check", "Create Button Works");
+                } catch (NumberFormatException exc) {
+                    Log.i("Valid inputs", "Invalid inputs");
+
+                }
             }
         });
 
